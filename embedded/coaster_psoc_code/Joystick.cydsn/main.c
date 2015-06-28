474196e16eb5bd773aa2db55d2afb2a8def3f386
Robert Barron <robertb431@gmail.com>
2015-06-27 20:54:00 -0400

    switched hid output to 5 buttons. added input for 4 load cellsand removed opamp

--- embedded/coaster_psoc_code/Joystick.cydsn/main.c
+++ embedded/coaster_psoc_code/Joystick.cydsn/main.c
@@ -1,154 +1,114 @@
-/*  --------------------------------------------------------------------------
-* Copyright 2010, Cypress Semiconductor Corporation.
+/*******************************************************************************
+* File Name: main.c
 *
-* This software is owned by Cypress Semiconductor Corporation (Cypress)
-* and is protected by and subject to worldwide patent protection (United
-* States and foreign), United States copyright laws and international
-* treaty provisions. Cypress hereby grants to licensee a personal,
-* non-exclusive, non-transferable license to copy, use, modify, create
-* derivative works of, and compile the Cypress Source Code and derivative
-* works for the sole purpose of creating custom software in support of
-* licensee product to be used only in conjunction with a Cypress integrated
-* circuit as specified in the applicable agreement. Any reproduction,
-* modification, translation, compilation, or representation of this
-* software except as specified above is prohibited without the express
-* written permission of Cypress.
-* 
-* Disclaimer: CYPRESS MAKES NO WARRANTY OF ANY KIND,EXPRESS OR IMPLIED,
-* WITH REGARD TO THIS MATERIAL, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-* Cypress reserves the right to make changes without further notice to the
-* materials described herein. Cypress does not assume any liability arising
-* out of the application or use of any product or circuit described herein.
-* Cypress does not authorize its products for use as critical components in
-* life-support systems where a malfunction or failure may reasonably be
-* expected to result in significant injury to the user. The inclusion of
-* Cypress' product in a life-support systems application implies that the
-* manufacturer assumes all risk of such use and in doing so indemnifies
-* Cypress against all charges.
-* 
-* Use may be limited by and subject to the applicable Cypress software
-* license agreement.
-* -----------------------------------------------------------------------------
-* Copyright (c) Cypress MicroSystems 2000-2003. All Rights Reserved.
+* Version: 2.1
 *
-*****************************************************************************
-*  Project Name: Joystick
-*  Project Revision: 1.00
-*  Software Version: PSoC Creator 2.0
-*  Device Tested: CY8C3866AXI
-*  Compilers Tested: Keil , ARM GCC
-*  Related Hardware: CY8CKIT-001
-*****************************************************************************
-***************************************************************************** */
-
-/* Project Description:
- * This project will create a simple USB HID joystick that can be used in PC gaming applications.
-***************************************************************************** */
+* Description:
+*  This project enumarates on the computer as a 3-button  mouse. 
+*  When the code is run, mouse cursor moves from the right to the left. 
+*
+* Refer to description on schematic for test procedure.
+*
+* Related Document:
+*  Device Class Definition for Human Interface Devices (HID)
+*  Firmware Specification- Version 1.11
+*
+********************************************************************************
+* Copyright 2012, Cypress Semiconductor Corporation. All rights reserved.
+* This software is owned by Cypress Semiconductor Corporation and is protected
+* by and subject to worldwide patent and copyright laws and treaties.
+* Therefore, you may use this software only as provided in the license agreement
+* accompanying the software package from which you obtained this software.
+* CYPRESS AND ITS SUPPLIERS MAKE NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* WITH REGARD TO THIS SOFTWARE, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT,
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*******************************************************************************/
 
 #include <device.h>
-#include <stdio.h>
 
-void StartUp (void); 		/* Function prototype for component startup API */
-void ReadJoystick (void);   /* Function prototype for reading and altering data for joystick information */
-void ReadButtons (void);    /* Function prototype for reading and altering data for button information */
+#define MOUSE_ENDPOINT      (0x01u)
+#define MOUSE_DATA_LEN      (0x03u)
+#define CURSOR_STEP         (0x05u)     /* Step size */
 
-static uint16 X_Axis=0, Y_Axis=0;
-static int16 X_Data, Y_Data;
-static int8 Joystick_Data[3] = {0, 0, 0}; /*[0] = X-Axis, [1] = Y-Axis, [2] = Buttons */
-static unsigned char Buttons;
+/* Mouse packet array: button, X, Y */
+uint8 mouseData[MOUSE_DATA_LEN] = {0u, 0u, 0u}; 
+uint8 bSNstring[16]={0x0Eu, 0x03u, 'F', 0u, 'W', 0u, 'S', 0u, 'N', 0u, '0', 0u, '1', 0u};
+
+uint8 FortyToByte(uint8 oz);
 
 void main()
 {
-    StartUp(); 								/* Calls the proper start API for all the components */
-	
-	for(;;)
+    uint8 counter = 0u;
+    
+    /* Enable Global Interrupts */
+    CyGlobalIntEnable;
+    
+    /* Set user defined Serial Number string descriptor */
+    //USBFS_1_SerialNumString(&bSNstring[0u]);
+    
+    /* Start USBFS device 0 with 3V operation */
+    USBFS_1_Start(0u, USBFS_1_5V_OPERATION); 
+
+    /* Wait for Device to enumerate */
+    while(!USBFS_1_GetConfiguration());
+
+    /* Enumeration is done, load endpoint 1. Do not toggle the first time. */
+    USBFS_1_LoadInEP(MOUSE_ENDPOINT, mouseData, MOUSE_DATA_LEN);
+    
+    while(1)
     {
-    	while(!USBFS_1_bGetEPAckState(1)); 	/* Wait for ACK before loading data */
-		ReadJoystick();						/* Calls function to read joystick movement */
-		ReadButtons();						/* Calls function to monitor button presses */
-		
-		Joystick_Data[0] = X_Data;		
-		Joystick_Data[1] = Y_Data;
-		Joystick_Data[2] = Buttons;
-		
-		USBFS_1_LoadInEP(1, (uint8 *)Joystick_Data, 3); /* Load latest mouse data into EP1 and send to PC */
+        /* Wait for ACK before loading data */
+        while(!USBFS_1_GetEPAckState(MOUSE_ENDPOINT));
+        
+        /* ACK has occurred, load the endpoint and toggle the data bit */
+        USBFS_1_LoadInEP(1u, mouseData, MOUSE_DATA_LEN);
+        
+        /* When our counts hits 128 */
+        if(counter == 128u)
+        {
+            /* Start moving the mouse to the right */
+            mouseData[1u] = 40u;
+            //mouseData[2u] |= 0x01;
+            //mouseData[2u] &= ~0x01;
+            
+            mouseData[2u] |= 0x01;
+            mouseData[2u] |= 0x02;
+            mouseData[2u] |= 0x04;
+            //mouseData[2u] |= 0x03;
+            //mouseData[2u] &= ~0x03;
+            //mouseData[2u] |= 0x04;
+            //mouseData[2u] &= ~0x04;
+            //mouseData[2u] |= 0x05;
+            //mouseData[2u] &= ~0x05;
+        }
+        else if(counter == 255u)
+        {
+            /* Start moving the mouse to the left */
+            //mouseData[1u] = (uint8)-(int8)CURSOR_STEP;
+            //mouseData[2u] &= ~0x01;
+            //mouseData[2u] &= ~0x02;
+            mouseData[2u] &= ~0x04;
+            mouseData[1u] = 0u;
+        }
+        //CyDelay(200);
+        /*mouseData[2u] &= ~0x02;
+        CyDelay(200);
+        mouseData[2u] &= ~0x03;
+        CyDelay(200);
+        mouseData[2u] &= ~0x04;
+        CyDelay(200);
+        mouseData[2u] &= ~0x05;
+        CyDelay(200);*/
+        //}
+
+        counter++;
     }
 }
 
-void StartUp (void)
+uint8 FortyToByte(uint8 oz)
 {
-	CYGlobalIntEnable;           					/* Enable Global interrupts */
-    ADC_DelSig_1_Start();        					/* Initialize ADC */
-    CyIntEnable(ADC_DelSig_1_IRQ__INTC_NUMBER ); 	/* Enables ADC interrupts */
-	AMux_1_Start( );         						/* Reset all channels */
-    AMux_1_Select(0);        						/* Connect channel 0 */
-	LCD_Char_1_Start(); 							/* Calls the start API for the Character LCD */
-	LCD_Char_1_Position(0,0);						/* Moves LCD cursor to Row 0, Column 0 */
-	LCD_Char_1_PrintString("PSoC 3 USB HID");
-	LCD_Char_1_Position(1,0);
-	LCD_Char_1_PrintString("Joystick Demo");
-	USBFS_1_Start(0, USBFS_1_DWR_VDDD_OPERATION);	/* Start USBFS operation/device 0 and with 5V operation */
-	while(!USBFS_1_bGetConfiguration());			/* Wait for Device to enumerate */
-    USBFS_1_LoadInEP(1, (uint8 *)Joystick_Data, 3); /* Loads an inital value into EP1 and sends it out to the PC */
+    
 }
 
-void ReadJoystick (void)
-{
-	AMux_1_Select(0);											/* Connects AMUX to channel 0 to connect ADC to joystick X-axis */  		
-	ADC_DelSig_1_StartConvert();								/* End ADC conversion */
-	ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_WAIT_FOR_RESULT); /* Wait for ADC reading to complete */
-    ADC_DelSig_1_StopConvert();									/* End ADC conversions */
-	X_Axis = ADC_DelSig_1_GetResult16();						/* Get ADC reading and store in variable X_Axis */
-	
-	AMux_1_Select(1);											/* Connects AMUX to channel 1 to connect ADC to joystick Y-axis */
-	ADC_DelSig_1_StartConvert();								/* Start ADC conversions */
-	ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_WAIT_FOR_RESULT); /* Wait for ADC reading to complete */
-    ADC_DelSig_1_StopConvert();									/* End ADC conversions */
-	Y_Axis = ADC_DelSig_1_GetResult16();						/* Get ADC reading and store in variable Y_Axis */
-	
-	X_Data = X_Axis - 127;										/* Adjust axis to center joystick */
-	Y_Data = Y_Axis - 127;
-	
-	if(X_Data > 127)										
-	X_Data = 127;
-	if(Y_Data > 127)
-	Y_Data = 127;
-	if(X_Data < -127)
-	X_Data = -127;
-	if(Y_Data< -127)
-	Y_Data = -127;
-	
-	Y_Data = Y_Data * -1;										/* Inverts Y-Axis for PC direction formatting */
-}
 
-void ReadButtons (void)
-{
-	if(Thumb_Button_Read() != 0)								/* Detect if button was pressed	*/
-	Buttons |= 0x01;											/* If pressed, mask bit to indicate button press */
-	else
-	Buttons &= ~0x01;											/* If released, clear bit */
-	 
-	if(Button_A_Read() != 0)
-	Buttons |= 0x02;
-	else
-	Buttons &= ~0x02;
-	
-	if(Button_B_Read() != 0)
-	Buttons |= 0x04;
-	else
-	Buttons &= ~0x04;
-	
-	if(Button_C_Read() != 0)
-	Buttons |= 0x08;
-	else
-	Buttons &= ~0x08;
-	
-	if(Button_D_Read() != 0)
-	Buttons |= 0x10;
-	else
-	Buttons &= ~0x10;
-}
-
-/* End of File */
-
+/* [] END OF FILE */
--- embedded/coaster_psoc_code/USB_UART01.cydsn/main.c
+++ embedded/coaster_psoc_code/USB_UART01.cydsn/main.c
@@ -1,37 +1,16 @@
- /*******************************************************************************
+/*******************************************************************************
 * File Name: main.c
 *
-* Version: 1.0
+* Version: 40.0
 *
 * Description:
-*   Enumerates as a Virtual Com port.  Receives data from hyper terminal, then 
-*   send received data backward. LCD shows the Line settings.
-*   
-*  To test project:
-*   1. Build the project and program the hex file on to the target device.
-*   2. Select 3.3V in SW3 and plug-in power to the CY8CKIT-001
-*   3. Connect USB cable from the computer to the CY8CKIT-001.
-*   4. Select the USB_UART.inf file from the project directory, as the driver 
-*      for this example once Windows asks for it.
-*   5. Open "Device Manager" and note the COM port number for "Example Project"
-*      device.
-*   6. Open "HyperTerminal" application and make new connection to noted COM port
-*   7. Type the message, observe echo data received.
+*   Reads from 2 pairs of 3-wire load cells to get a 40 oz value and maps these to 
+*   hid output (5-bit number represented as 5 joystick buttons)
 *
-* Related Document:
-*  Universal Serial Bus Specification Revision 2.0 
-*  Universal Serial Bus Class Definitions for Communications Devices 
-*  Revision 1.2
+*   Currently sends adc and 40 value over usb uart for debugging
 *
 ********************************************************************************
-* Copyright 2012, Cypress Semiconductor Corporation. All rights reserved.
-* This software is owned by Cypress Semiconductor Corporation and is protected
-* by and subject to worldwide patent and copyright laws and treaties.
-* Therefore, you may use this software only as provided in the license agreement
-* accompanying the software package from which you obtained this software.
-* CYPRESS AND ITS SUPPLIERS MAKE NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-* WITH REGARD TO THIS SOFTWARE, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT,
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+* High Gravity Solutions
 *******************************************************************************/
 
 #include <device.h>
@@ -48,39 +27,41 @@
 *  IN and OUT bulk endpoints. 
 */
 #define BUFFER_LEN  64u
-#define NUM_SAMPLES 50
+#define NUM_SAMPLES 30
+#define EMPTY 0
+#define FULL 1
 
 char8 *parity[] = { "None", "Odd", "Even", "Mark", "Space" };
 char8 *stop[] = { "1", "1.5", "2" };
 
 /* Function that encapsulates the process of writing text strings to USBUART */
 void PrintToUSBUART(char8 * outText);
-uint8 convertToOunces(uint16 calibration, uint16 counts);
+int8 convertToOunces(uint16 calibrate_full, uint16 calibrate_empty, uint16 counts);
+uint16 calibrateTo40();
 
-void main()
+int main()
 {
     uint16 count;
     uint8 buffer[BUFFER_LEN];
     char8 lineStr[20];
     uint8 state;
-     uint16 adcReading = 0u;
+    uint16 adcReading = 0u;
     //uint16 adc_buffer[NUM_SAMPLES];
     uint32 adc_buffer = 0u;
     float volts = 0;
     uint8 ounces = 40; 
+    uint8 button_state = 0;
+    uint8 last_button_state = 2;
+    uint16 empty_value = 500;
+    uint16 full_value =  1500;
     /* Enable Global Interrupts */
     CyGlobalIntEnable;                        
     //ADC_SAR_1_Start();
     adc_1_Start();
-    /* Start USBFS Operation with 3V operation */
+    AMuxSeq_1_Start();
+    /* Start USBFS Operation with 5V operation */
     USBUART_1_Start(0u, USBUART_1_5V_OPERATION);
-    /* Starts PGA component */
-    //PGA_1_Start();
-//    PGA_Inv_1_Start();
-    /* Start LCD */
-    LCD_Start();
-    LCD_PrintString("USB-UART example");
-    PGA_1_Start();
+    
     /* Wait for Device to enumerate */
     while(!USBUART_1_GetConfiguration());
 
@@ -88,61 +69,81 @@ void main()
     USBUART_1_CDC_Init();
     
     char add[50];
+    int i = 0;
     adc_1_StartConvert();
     /* Main Loop: */
     for(;;)
-    {
-        //PrintToUSBUART("Please choose the channel (0-7) \n\r");
-        //if(USBUART_1_DataIsReady() != 0u)               /* Check for input data from PC */
-        //{   
-        //ADC_SAR_1_StartConvert();
+    { 
+        button_state = button_Read();
         
-        //ADC_SAR_1_IsEndConversion(ADC_SAR_1_WAIT_FOR_RESULT);
-        //adcReading = ADC_SAR_1_GetResult16(); 
-        int i = 0;
+        if(button_state != last_button_state)
+        {
+            if(button_state == 0)
+                empty_value = calibrateTo40();  
+            else
+                full_value = calibrateTo40();
+        }
+        
+        i = 0;
         adc_buffer = 0;
         for(i = 0; i < NUM_SAMPLES; i++)
         {
             adc_buffer += adc_1_Read16();
         }
         adcReading = adc_buffer/NUM_SAMPLES;
-        ounces = convertToOunces(100,adcReading);
-        //volts = adc_1_CountsTo_Volts(adcReading);
+        ounces = convertToOunces(full_value,empty_value,adcReading);
+        volts = adc_1_CountsTo_Volts(adcReading);
         sprintf(add,"counts: %hu\r\n",adcReading);
         PrintToUSBUART(add);
+        //sprintf(add,"counts: %f\r\n",volts);
+        //PrintToUSBUART(add);
         sprintf(add,"ounces: %i\r\n",ounces);
         PrintToUSBUART(add);
         
+        //Not drinking...
+        if(ounces >= 0)
+        {
+            //update ounce value
+        }
+        last_button_state = button_state;
         CyDelay(200);
        // }
     }   
 }
 
-uint8 convertToOunces(uint16 calibration,uint16 counts)
+int8 convertToOunces(uint16 calibrate_full, uint16 calibrate_empty, uint16 counts)
 {
     //take initial calibration value as 40 ounces
     //stored 0 ounce value
     uint16 empty = 6500;
     uint16 full = 6417;
     
-    float current = 40 - ((counts - full)/2); 
+    //float current = 40 - ((counts - full)/2); 
+    float current = 40 - ((counts - full)/((empty-full)/40));
     
     return (uint8)current;
 }
 
-//Calibrate to initial full 40 value
+//Calibrate to initial full 40 value on boot
 uint16 calibrateTo40()
 {
     uint16 adcReading = 0u;
     //uint16 adc_buffer[NUM_SAMPLES];
-    uint32 adc_buffer = 0u;
+    uint32 adc_buffer_1 = 0u; //load cell pair one
+    uint32 adc_buffer_2 = 0u; //load cell pair two
     int i = 0;
-    adc_buffer = 0;
+    //adc_buffer = 0;
     for(i = 0; i < NUM_SAMPLES; i++)
     {
-        adc_buffer += adc_1_Read16();
+        if(AMuxSeq_1_GetChannel() == 0)
+            adc_buffer_1 += adc_1_Read16();
+        else if(AMuxSeq_1_GetChannel() == 1)
+            adc_buffer_2 += adc_1_Read16();
+            
+        AMuxSeq_1_Next();
     }
-    adcReading = adc_buffer/NUM_SAMPLES;
+    
+    adcReading = ((adc_buffer_2/NUM_SAMPLES)+(adc_buffer_1/NUM_SAMPLES))/2;
     
     return adcReading;
 }
